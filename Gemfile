7f4f8c4e8b08e5275cdacc37859faec8c397b179
LuisEduardoAlmeida <leduardoalmeida@hotmail.com>
2018-09-03 11:48:52 -0300

    Adding api version constraint to routes

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ group :development, :test do
   gem 'rspec-rails', '~> 3.5'
 end
 
+
 group :test do
   gem 'database_cleaner'
   gem 'shoulda-matchers', '~> 3.1'
---
+++ lib/api_version_constraint.rb
@@ -0,0 +1,10 @@
+class ApiVersionConstraint	
+  def initialize(options)
+    @version  = options[:version]
+    @defaults  = options[:defaults]
+  end
+
+  def matches?(req)
+    @defaults || req.headers['Accept'].include?("application/vnd.taskmanager.v#{@version} ")
+  end
+end
